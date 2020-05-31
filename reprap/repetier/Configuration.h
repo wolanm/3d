0f8ed43d2b04a7abb62cf147485a8fbd5e64d89d
michal.wolanin <michal.wolanin@asseco.pl>
2020-05-31 20:11:05 +0200

    test

--- reprap/repetier/Configuration.h
+++ reprap/repetier/Configuration.h
@@ -13,7 +13,7 @@
 
     You should have received a copy of the GNU General Public License
     along with Repetier-Firmware.  If not, see <http://www.gnu.org/licenses/>.
-
+    //MWO
 */
 
 #ifndef CONFIGURATION_H
